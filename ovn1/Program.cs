c04936ce6ce55be3706ac2190feaafc328a5abdf
Karin Skans <skka19st@student.ju.se>
2019-11-11 15:58:24 +0100

    ovn 1_Lista fem namn_Version01

---
+++ ovn1/Program.cs
@@ -0,0 +1,28 @@
+﻿// namespace som används
+using System;
+using System.Collections.Generic;
+
+namespace ovn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> djurlista = new List<string>();
+            Console.WriteLine("Ange namn på fem djur:");
+
+            for (int ind = 1 ; ind <= 5 ; ind++)
+            {
+                Console.Write(ind + ": ");
+                string djurnamn = Console.ReadLine();
+                djurlista.Add(djurnamn);
+            }
+
+            Console.WriteLine("Dessa djur finns nu i listan: ");
+            foreach (string namn in djurlista)
+            {
+                Console.WriteLine(namn);
+            }
+        }
+    }
+}
